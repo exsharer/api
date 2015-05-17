40f7ecb00f11bab6ae10a7b46ec0a82bc4005ad6
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-17 08:05:42 -0500

    [Project] Added controller-level

---
+++ controllers/project.js
@@ -0,0 +1,27 @@
+module.exports = function(){
+
+    var dao     = new app.dao.project()
+    ,   json    = new app.views.json()
+    ,   query   = app.utils.mongoose.query;
+
+    return {
+        list: function (req, res, next) {
+            var input = query(req.query);
+
+            return dao.list(
+                input.query, input.fields, input.projection
+            ).then(function(data){
+                json.standard(data, res);
+            }).catch(next);
+        },
+        me: function(req, res, next){
+            json.standard(req.user, res);
+        },
+        create: function(req, res, next){
+            dao.create(req.body).then(function(data){
+                json.standard(data, res);
+            }).catch(next);
+        }
+    };
+
+};
