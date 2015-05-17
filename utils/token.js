af93b75903ec598db4bd9127d642d421e4ab2873
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-17 11:20:24 -0500

    Change encoding algorithm for creating tokens

--- utils/token.js
+++ utils/token.js
@@ -4,7 +4,7 @@ module.exports = function(size){
 
     var crypto = require('crypto');
 
-    return crypto.createHash('sha512').update(
+    return crypto.createHmac('sha512', new Date().toString()).update(
         crypto.randomBytes(size)
     ).digest('base64');
 
