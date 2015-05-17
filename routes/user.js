3ea88e2a3bb9fb83f24f20d2caa6b2400a8d7a6d
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-17 06:44:34 -0500

    [User] Filling routes

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,20 @@ module.exports = function(){
     return [
         {
             method: 'get',
+            url: '/users',
+            action: users.list,
+            cors: true
+        },
+        {
+            method: 'get',
             url: '/users/me',
             action: user.me,
+            cors: true
+        },
+        {
+            method: 'post',
+            url: '/users',
+            action: users.create,
             cors: true
         }
     ];
