3b7c2d9f0bce5f4d0bf649e3c8992b73ea3d339b
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-17 10:06:41 -0500

    Test to create users

--- test/user.js
+++ test/user.js
@@ -5,10 +5,25 @@ describe('Users module', function(){
 
     var Client  = new app.dao.client()
     ,   User    = app.db.User
-    ,   Dao     = app.dao.user;
+    ,   Dao     = new app.dao.user()
+    ,   sha1    = app.utils.sha1;
 
     describe('DAO level', function(){
         before(function(done){ User.collection.remove(done); });
+
+        it('Creates an user', function(){
+            return Dao.create({
+                username: "booler",
+                email: "booler@boolinc.co",
+                password: sha1("b00l3r"),
+                name: {
+                    first: "Another",
+                    middle: "Other",
+                    last: "Booler"
+                },
+                birthday: new Date("1994-05-07")
+            });
+        });
     });
 
     describe('Controller level', function(){
