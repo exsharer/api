4b7d206a9b79a32b45a8d25739036fd98ad0872d
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-17 13:22:05 -0500

    Delete comments

--- security/oauth2/server.js
+++ security/oauth2/server.js
@@ -3,37 +3,29 @@
 var Token   = new app.dao.token()
 ,   User    = new app.dao.user();
 
-// Create OAuth 2.0 Authorization Server
 var oauth2orize = app.utils.oauth2orize
 ,   passport    = app.utils.passport
 ,   server      = oauth2orize.createServer();
 
-// Exchange username & password for access token.
 server.exchange(oauth2orize.exchange.password(
     function(client, username, password, scope, done) {
-
         User.login(username, password).then(function(user){
             return user ? Token.create(user._id, client._id) : false;
         }).then(function(token){
             done(null, token.access || false, token.refresh);
         }).catch(done);
-
     }
 ));
 
-// Exchange refreshToken for access token.
 server.exchange(oauth2orize.exchange.refreshToken(
     function(client, refreshToken, scope, done) {
-
         Token.refresh(refreshToken).then(function(data){
             if(!data) return done(null, false);
             return Token.create(data.user, data.client);
         }).catch(done);
-
     }
 ));
 
-// Token authorization server endpoint
 module.exports = [
     passport.authenticate(['oauth2-client-password'], { session: false }),
     server.token(),
