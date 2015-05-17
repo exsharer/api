109c2ad87bb15ea725de61cb1ffdf71dc2be51e6
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-17 06:42:58 -0500

    [User] Filling controller-level methods

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,26 @@
 module.exports = function(){
 
-    var json = new app.views.json();
+    var dao     = new app.dao.user()
+    ,   json    = new app.views.json()
+    ,   query   = app.utils.mongoose.query;
 
     return {
+        list: function (req, res, next) {
+            var input = query(req.query);
+
+            return dao.list(
+                input.query, input.fields, input.projection
+            ).then(function(data){
+                json.standard(data, res);
+            }).catch(next);
+        },
         me: function(req, res, next){
             json.standard(req.user, res);
+        },
+        create: function(req, res, next){
+            dao.create(req.body).then(function(data){
+                json.standard(data, res);
+            }).catch(next);
         }
     };
 
