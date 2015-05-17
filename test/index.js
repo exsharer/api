d87f4059e45adc1990786d63f98c2bab6f975284
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-17 07:10:45 -0500

    Manage assertions via chai-as-promised

--- test/index.js
+++ test/index.js
@@ -2,10 +2,17 @@ global.app      = require('..');
 global._        = app.utils._;
 global.Q        = app.utils.q;
 global.log      = app.utils.log;
-global.expect   = require('expect.js');
+global.util     = require('util');
+
+var chai        = require('chai')
+,   asPromised  = require('chai-as-promised');
+chai.use(asPromised);
+
+global.expect   = chai.expect || require('expect.js');
+
 global.progress = require('progress');
 global.async    = require('async');
-global.util     = require('util');
+
 global.request  = require('supertest')(app.application);
 
 require('./client');
