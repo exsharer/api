9edff0e9dfa2993e7719eb9795df88e78424f891
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-17 06:27:02 -0500

    Cleaning test code in project

--- test/project.js
+++ test/project.js
@@ -1,15 +1,15 @@
 describe('Project module', function(){
 
     var Project = app.db.Project
-    ,   Dao     = app.dao.project;
+    ,   Dao     = new app.dao.project();
 
     describe('DAO level', function(){
 
         before(function(done){ Project.collection.remove(done) });
 
-        it('Creates a proejct', function(){
+        it('creates a project', function(){
 
-            return new Dao().create({
+            return Dao.create({
                 title: "My awesome project",
                 description: "Incididunt ullamco aute ea minim commodo qui ipsum ut incididunt nisi magna ipsum commodo.",
                 place: {
